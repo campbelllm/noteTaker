21d7349616dba68f7bb30183d7f80f6c6302490c
Lindsey Smith <linds@lindseys-mbp.lan>
2020-09-24 23:48:42 -0600

    retrieved new note and wrote to file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -12,6 +12,8 @@ const PORT = 8080;
 app.use(express.json())
 // for any request check this static (never changing, not moving, css, html, js) to see if there is anything request needs
 app.use(express.static('../public'))
+//telling express what to kind of data to expect on the request
+app.use(express.urlencoded({extended: true}));
 
 //route requests
 app.get('/', (req, res) => {
@@ -28,6 +30,32 @@ app.get('/api/notes', (req, res) => {
         if (err) throw err;
         res.send(JSON.parse(notes))
     })
+});
+
+app.post('/api/notes', (req, res) => {
+    //grabbing current notes from db.json
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    //grabbing last id in array
+    let id = notes[notes.length -1].id
+    //if there is no id yet, set id to 0
+    if(!id){
+        id = 0;
+    //otherwise set id to last id in array + 1
+    }else{
+        id++;
+    }
+    // here we are adding the new note typed in, assigning values to a var
+    const newNote =  req.body
+    //giving the new not and id
+    newNote.id = id
+    //push new not to notes array (what we grabbed from the file above)
+    notes.push(newNote)
+    const noteStringify = JSON.stringify(notes)
+    //write file with new note added
+    fs.writeFile('../db/db.json', noteStringify, (err) => {
+        if(err) throw err;
+    })
+    res.status(400)
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
