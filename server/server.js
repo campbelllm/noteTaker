acfaa16c86d68613bc4c1a159aa060ece9bc4a4a
Lindsey Smith <linds@lindseys-mbp.lan>
2020-09-27 22:08:26 -0600

    adding port.env

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const notesController = require("./notesController");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //middlware
 app.use(express.json());
